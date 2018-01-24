887acdcebc93f69270b4adb01a315e07069d56e4
pli01 <pli01@github.com>
2018-01-24 18:32:15 +0000

    enable gitlab, disable sonarqube

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+sudo docker pull sonatype/nexus3
+sudo docker pull gitlab/gitlab-ce:latest
+sudo docker pull jenkins/jenkins:lts
+
+export http_proxy
+export https_proxy
+export no_proxy
+
+sudo -E docker-compose up -d
+
