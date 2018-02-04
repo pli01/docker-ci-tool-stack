d88845bd1d9e0c0171b693bf978af8c0f9fdddce
pli <philippe.libat@free.fr>
2018-02-04 12:15:34 +0100

    run.sh exit on error

--- service-config/run.sh
+++ service-config/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
- echo "Service configuration started"
-time ( cd /opt/ansible &&
+set -e
+echo "Service configuration started"
+time ( cd $(dirname $0)/ansible &&
   ansible-playbook -i config -c local -l localhost playbooks/nexus.yml
 )
- echo "Service configuration finished"
+echo "Service configuration finished"
