5390015e60c2e80c05fb55421de1e5fe6a4d9d79
pli01 <pli01@github.com>
2018-02-09 17:58:41 +0000

    jenkins: docker-compose, groovy with env var

--- jenkins/init.groovy.d/executor.groovy
+++ jenkins/init.groovy.d/executor.groovy
@@ -1,2 +1,7 @@
 import jenkins.model.*
-Jenkins.instance.setNumExecutors(5)
+
+// get Env var
+def env = System.getenv()
+
+def jenkins_instance_setNumExecutors = env['jenkins_instance_setNumExecutors'] ?: "5"
+Jenkins.instance.setNumExecutors(jenkins_instance_setNumExecutors)
