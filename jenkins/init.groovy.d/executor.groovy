b0aea24fec0272cb8aef1ef387cce3a4f5076245
pli <philippe.libat@free.fr>
2018-02-10 16:11:18 +0100

    Use Env Var at run time to configure jenkins

--- jenkins/init.groovy.d/executor.groovy
+++ jenkins/init.groovy.d/executor.groovy
@@ -1,7 +1,11 @@
+import jenkins.*
 import jenkins.model.*
+import hudson.*
+import hudson.model.*
 
 // get Env var
 def env = System.getenv()
+def jenkins_instance_setNumExecutors = Integer.parseInt( env['JENKINS_INSTANCE_SETNUMEXECUTORS'] ?: "5" )
 
-def jenkins_instance_setNumExecutors = env['jenkins_instance_setNumExecutors'] ?: "5"
-Jenkins.instance.setNumExecutors(jenkins_instance_setNumExecutors)
+Jenkins.instance.setNumExecutors( jenkins_instance_setNumExecutors )
+Jenkins.instance.save()
