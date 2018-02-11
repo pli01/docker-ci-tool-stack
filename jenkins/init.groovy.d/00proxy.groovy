8e3a34ac4672d8dc72a58d2ecbcf5fbbaa63426f
pli <philippe.libat@free.fr>
2018-02-11 17:11:01 +0100

    fix proxy host/port empty

--- jenkins/init.groovy.d/00proxy.groovy
+++ jenkins/init.groovy.d/00proxy.groovy
@@ -6,7 +6,7 @@ def env = System.getenv()
 // need git plugins
 def httpProxyHost = env['HTTP_PROXY_HOST'] ?: ""
 //def httpProxyPort = Integer.parseInt( env['HTTP_PROXY_PORT'] ) ?: 0
-def httpProxyPort = env['HTTP_PROXY_PORT'] ?: "8888"
+def httpProxyPort = env['HTTP_PROXY_PORT'] ?: ""
 //int httpProxyPort = env['HTTP_PROXY_PORT'].toInteger() ?: 8888
 def httpProxyUser = env['HTTP_PROXY_USER'] ?: ""
 def httpProxyPassword = env['HTTP_PROXY_PASSWORD'] ?: ""
@@ -17,14 +17,18 @@ def instance = Jenkins.getInstance()
 
 Thread.start {
     // proxy config
+    if ( httpProxyHost.isEmpty() || httpProxyPort.isEmpty()) {
+      println "--> No Proxy config"
+      return
+    }
     println "--> Configuring Proxy config"
     final String name = httpProxyHost
-    final int port = httpProxyPort.toInteger()
+    final int port =  httpProxyPort.toInteger()
     final String userName = httpProxyUser
     final String password = httpProxyPassword
-    final String noProxyHost = httpProxyExceptions
+    final String noProxyHost = httpProxyExceptions.replace(',','\r\n')
 
-    final def pc = new hudson.ProxyConfiguration(name, port, userName, password, noProxyHost)
+    final def pc = new hudson.ProxyConfiguration( name, port , userName, password, noProxyHost)
     instance.proxy = pc
     // Save the state
     instance.save()
